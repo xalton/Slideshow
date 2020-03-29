bbe4bbdf01004892b5e54f219effaab8243c7fad
Ibrahim Khaled <ikhaled@ulb.ac.be>
2020-03-29 15:04:27 +0200

    files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function setUpEvents() {
+  var time = 1000;
+  var path = 'img\\'
+  var i = 1;
+
+  function changeImg() {
+    var imageData = new Image();
+    imageData.onload = function() {
+      document.slide.src = path + i.toString() + '.jpg';
+      i = i + 1
+      setTimeout(changeImg, time);
+    }
+    imageData.onerror = function() {
+      i = 1;
+      changeImg();
+    }
+    imageData.src = path + i.toString() + '.jpg';
+  }
+  changeImg();
+}
+
+window.onload = function() {
+  setUpEvents();
+}
